5dfc5e1dbe2c18c5e3d40de0011a930037bf2891
Carlo Contardi <c.contardi@spacecoding.it>
2024-12-31 17:00:09 +0100

    [Feature] printing numbers to the screen

--- app/src/main/java/it/spacecoding/calculator_android/MainActivity.java
+++ app/src/main/java/it/spacecoding/calculator_android/MainActivity.java
@@ -8,17 +8,58 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import it.spacecoding.calculator_android.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding mainBinding;
+    String number = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        mainBinding.tvResult.setText("0");
+        mainBinding.btn0.setOnClickListener(v -> {
+            onNumberClicked("0");
         });
+        mainBinding.btn1.setOnClickListener(v -> {
+            onNumberClicked("1");
+        });
+        mainBinding.btn2.setOnClickListener(v -> {
+            onNumberClicked("2");
+        });
+        mainBinding.btn3.setOnClickListener(v -> {
+            onNumberClicked("3");
+        });
+        mainBinding.btn4.setOnClickListener(v -> {
+            onNumberClicked("4");
+        });
+        mainBinding.btn5.setOnClickListener(v -> {
+            onNumberClicked("5");
+        });
+        mainBinding.btn6.setOnClickListener(v -> {
+            onNumberClicked("6");
+        });
+        mainBinding.btn7.setOnClickListener(v -> {
+            onNumberClicked("7");
+        });
+        mainBinding.btn8.setOnClickListener(v -> {
+            onNumberClicked("8");
+        });
+        mainBinding.btn9.setOnClickListener(v -> {
+            onNumberClicked("9");
+        });
+
+
+    }
+
+    public void onNumberClicked(String clickedNumber){
+        if(number == null){
+            number = clickedNumber;
+        }else{
+            number += clickedNumber;
+        }
+        mainBinding.tvResult.setText(number);
     }
 }
